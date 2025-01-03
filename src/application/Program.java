6bbd186d3f6804078f15ed21e640ef84530a0cf5
Oslain Gabriel <oslaingabriel9@gmail.com>
2025-01-03 15:21:11 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -7,8 +9,9 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findBYId(10);
+
         System.out.println(seller);
     }
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -17,6 +17,10 @@ public class Department implements Serializable {
         this.name = name;
     }
 
+    public Department(){
+
+    }
+
     public Integer getId() {
         return id;
     }
