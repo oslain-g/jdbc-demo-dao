071f0dd6749dee6c95393f9d4017a31d1ea51ee9
Oslain Gabriel <oslaingabriel9@gmail.com>
2025-01-03 13:08:43 -0300

    DepartmentDao and SellerDaos interfaces

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,10 +1,14 @@
 package model.entities;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
 public class Seller implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private Integer id;
     private String name;
     private String email;
