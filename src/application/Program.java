700d3fce3b53bba44f50ad9d18e9416f56ccaaa7
Oslain Gabriel <oslaingabriel9@gmail.com>
2025-01-05 17:16:13 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("==== TEST 1: seller findById ====");
@@ -34,10 +37,16 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
-        System.out.println("\n==== TEST 4: seller update ====");
+        System.out.println("\n==== TEST 5: seller update ====");
         seller = sellerDao.findBYId(1);
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update complete!");
+
+        System.out.println("\n==== TEST 6: seller delete ====");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed!");
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,5 +1,6 @@
 package model.dao.impl;
 
+import com.mysql.cj.x.protobuf.MysqlxCrud;
 import db.DB;
 import db.DbException;
 import model.dao.SellerDao;
@@ -78,7 +79,19 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
 
+        try{
+            st = conn.prepareStatement("DELETE FROM seller WHERE id = ?");
+
+            st.setInt(1, id);
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
